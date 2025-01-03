5fcbd9e069e1897c3f257aeff87e5430b8c0cff3
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2025-01-03 19:41:05 +0100

    Adding more data to the mealdetails

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_detail.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -7,6 +8,14 @@ class MealsScreen extends StatelessWidget {
 
   final String title;
   final List<Meal> meals;
+
+  void selectMeal(
+    BuildContext context,
+    Meal meal,
+  ) {
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (ctx) => MealDetailScreen(meal: meal)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,8 +43,14 @@ class MealsScreen extends StatelessWidget {
     );
     if (meals.isNotEmpty) {
       content = ListView.builder(
-          itemCount: meals.length,
-          itemBuilder: (context, index) => MealItem(meal: meals[index]));
+        itemCount: meals.length,
+        itemBuilder: (context, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (context, meal) {
+            selectMeal(context, meal);
+          },
+        ),
+      );
     }
 
     return Scaffold(
