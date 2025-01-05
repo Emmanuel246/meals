256825cba38e625bce0d20eb9d54f70a19f20004
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2025-01-06 00:46:09 +0100

    Added Favourite Functionality

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -4,17 +4,18 @@ import 'package:meals/screens/meal_detail.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key, this.title, required this.meals, required this.onToggleFavorite});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
 
   void selectMeal(
     BuildContext context,
     Meal meal,
   ) {
     Navigator.of(context).push(
-        MaterialPageRoute(builder: (ctx) => MealDetailScreen(meal: meal)));
+        MaterialPageRoute(builder: (ctx) => MealDetailScreen(meal: meal, onToggleFavorite: onToggleFavorite,)));
   }
 
   @override
@@ -41,6 +42,7 @@ class MealsScreen extends StatelessWidget {
         ],
       ),
     );
+
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
@@ -52,10 +54,12 @@ class MealsScreen extends StatelessWidget {
         ),
       );
     }
-
+    if (title == null) {
+      return content;
+    }
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
